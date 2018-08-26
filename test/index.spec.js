be93fef4bb6bef864451acb6b4351020631ca4df
Kit Jonathan Te <kjonathante@gmail.com>
2018-08-25 23:24:28 -0700

    fix index.spec.js test

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,5 +4,6 @@ const server = require('../server.js');
 describe('test', () => {
   it('should return a string', () => {
     expect('ci with travis').to.equal('ci with travis');
+    server.close()
   });
 });
